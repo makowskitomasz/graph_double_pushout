6802eedaf31e82fbd2ef2bf451568918a9b4e230
tomasz.makowski.2 <tomasz.makowski.2@asseco.pl>
2024-11-14 20:41:32 +0100

    Guziki

--- callbacks.py
+++ callbacks.py
@@ -23,10 +23,17 @@ def register_callbacks(app, base_graph):
     ]
     
     @app.callback(
-        [Output('main-graph', 'elements', allow_duplicate=True),
-         Output('main-graph-data', 'data', allow_duplicate=True),
-         Output('graph-description', 'children', allow_duplicate=True),
-         Output('feedback', 'children', allow_duplicate=True)],
+        [Output('main-graph', 'elements'),
+         Output('main-graph-data', 'data'),
+         Output('graph-description', 'children'),
+         Output('feedback', 'children'),
+         Output('next-step-button', 'disabled'),
+         Output('previous-step-button', 'disabled'),
+         Output('graph-l', 'elements'),
+         Output('graph-k', 'elements'),
+         Output('graph-r', 'elements'),
+         Output('import-productions-button', 'contents'),
+         Output('apply-production-button', 'disabled')],
         [Input('add-node-button', 'n_clicks'),
          Input('add-edge-button', 'n_clicks'),
          Input('remove-selected-button', 'n_clicks'),
@@ -34,7 +41,10 @@ def register_callbacks(app, base_graph):
          Input('clear-graph-button', 'n_clicks'),
          Input('apply-production-button', 'n_clicks'),
          Input('next-step-button', 'n_clicks'),
-         Input('previous-step-button', 'n_clicks')],
+         Input('previous-step-button', 'n_clicks'),
+         Input('remove-production-button', 'n_clicks'),
+         Input('remove-all-productions-button', 'n_clicks'),
+         Input('import-productions-button', 'contents')],
         [State('main-graph', 'elements'),
          State('main-graph', 'selectedNodeData'),
          State('main-graph', 'selectedEdgeData'),
@@ -42,12 +52,11 @@ def register_callbacks(app, base_graph):
          State('graph-k', 'elements'),
          State('graph-r', 'elements'),
          State('main-graph-data', 'data')],
-        prevent_initial_call=True
     )
-    def update_graph(n_clicks_node, n_clicks_edge, n_clicks_remove, n_clicks_load, n_clicks_clear, n_clicks_apply, n_clicks_next, n_clicks_prev, elements, selected_nodes, selected_edges, l_elements, k_elements, r_elements, graph_data):
+    def update_graph(n_clicks_node, n_clicks_edge, n_clicks_remove, n_clicks_load, n_clicks_clear, n_clicks_apply, n_clicks_next, n_clicks_prev, n_clicks_remove_prod, n_clicks_remove_all, contents, elements, selected_nodes, selected_edges, l_elements, k_elements, r_elements, graph_data):
         ctx = dash.callback_context
         if not ctx.triggered:
-            return elements, graph_data, "", ""
+            return elements, graph_data, "", "", True, True, l_elements, k_elements, r_elements, dash.no_update, True
 
         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
@@ -55,7 +64,7 @@ def register_callbacks(app, base_graph):
             base_graph.copy_from(elements)
             node_id = str(len([element for element in base_graph.elements if 'source' not in element['data']]) + 1)
             base_graph.add_node(node_id)
-            return base_graph.elements, graph_data, descriptions[0], ""
+            return base_graph.elements, graph_data, descriptions[0], "", True, True, l_elements, k_elements, r_elements, dash.no_update, False
 
         elif button_id == 'add-edge-button':
             base_graph.copy_from(elements)
@@ -64,23 +73,22 @@ def register_callbacks(app, base_graph):
                     for j in range(i + 1, len(selected_nodes)):
                         source, target = selected_nodes[i]['id'], selected_nodes[j]['id']
                         base_graph.add_edge(source, target)
-            return base_graph.elements, graph_data, descriptions[0], ""
+            return base_graph.elements, graph_data, descriptions[0], "", True, True, l_elements, k_elements, r_elements, dash.no_update, False
 
         elif button_id == 'remove-selected-button':
             base_graph.copy_from(elements)
             base_graph.remove_elements(selected_nodes, selected_edges)
-            return base_graph.elements, graph_data, descriptions[0], ""
+            return base_graph.elements, graph_data, descriptions[0], "", True, True, l_elements, k_elements, r_elements, dash.no_update, False
 
         elif button_id == 'load-graph-button':
             from app import GRAPH_FILE_PATH
-            base_graph.from_csv('data/edges.csv')
             base_graph.from_csv(GRAPH_FILE_PATH)
             base_graph.elements = add_lock_to_all_graph_elements(base_graph.elements)
-            return base_graph.elements, graph_data, descriptions[0], ""
+            return base_graph.elements, graph_data, descriptions[0], "", True, True, l_elements, k_elements, r_elements, dash.no_update, False
 
         elif button_id == 'clear-graph-button':
             base_graph.clear()
-            return base_graph.elements, graph_data, descriptions[0], ""
+            return base_graph.elements, graph_data, descriptions[0], "", True, True, l_elements, k_elements, r_elements, dash.no_update, True
 
         elif button_id == 'apply-production-button':
             base_graph.copy_from(elements)
@@ -104,7 +112,7 @@ def register_callbacks(app, base_graph):
                     morphism[node['data']['id']] = node['data']['id']
                 dpo.define_morphism(morphism)
             else:
-                return dash.no_update, dash.no_update, "Please check the input graphs. Cannot apply the DPO production.", "Please check the input graphs. Cannot apply the DPO production."   
+                return dash.no_update, dash.no_update, "Please check the input graphs. Cannot apply the DPO production.", "Please check the input graphs. Cannot apply the DPO production.", True, True, l_elements, k_elements, r_elements, dash.no_update, False   
 
             mL_minus_mK = dpo.calculate_mL_minus_mK()
             graph_mL_minus_mK = Graph()
@@ -136,14 +144,69 @@ def register_callbacks(app, base_graph):
             G_prime_highlited_mR_minus_mK = add_lock_to_all_graph_elements(G_prime_highlited_mR_minus_mK)
 
             if not nx.is_weakly_connected(Z) or not nx.is_weakly_connected(G_prime):
-                return dash.no_update, dash.no_update, "Please check the input graphs. Cannot apply the DPO production.", "Please check the input graphs. Cannot apply the DPO production."            
+                return dash.no_update, dash.no_update, "Please check the input graphs. Cannot apply the DPO production.", "Please check the input graphs. Cannot apply the DPO production.", True, True, l_elements, k_elements, r_elements, dash.no_update, False            
 
             graph_elements = [base_graph.elements, G_highlight_L, G_highlight_L_minus_K, graph_Z.elements, G_prime_highlited_mR_minus_mK, graph_G_prime.elements]
 
             graph_elements = [fill_classes_as_empty_if_does_not_exist(i) for i in graph_elements]                
             graph_data = {'current_index': 0, 'graphs': graph_elements}
-            return graph_elements[0], graph_data, descriptions[0], ""
-        return dash.no_update, dash.no_update, "", ""
+            return graph_elements[0], graph_data, descriptions[0], "", False, True, l_elements, k_elements, r_elements, dash.no_update, False
+
+        elif button_id == 'next-step-button' or button_id == 'previous-step-button':
+            def can_go_backward(graph_data):
+                return graph_data['current_index'] > 0
+            def can_go_forward(graph_data):
+                return graph_data['current_index'] < len(graph_data['graphs']) - 1
+
+            if graph_data is None or 'graphs' not in graph_data:
+                return dash.no_update, dash.no_update, dash.no_update, dash.no_update, True, True, l_elements, k_elements, r_elements, dash.no_update, False
+
+            current_index = graph_data['current_index']
+            graphs = graph_data['graphs']
+
+            if button_id == 'next-step-button' and current_index + 1 < len(graphs):
+                current_index = (current_index + 1)
+            elif button_id == 'previous-step-button' and current_index - 1 >= 0:
+                current_index = (current_index - 1)
+
+            graph_data['current_index'] = current_index
+            return graphs[current_index], graph_data, descriptions[current_index], "", not can_go_forward(graph_data), not can_go_backward(graph_data), l_elements, k_elements, r_elements, dash.no_update, False
+
+        elif button_id == 'remove-production-button':
+            if base_graph.elements:
+                return base_graph.elements, {'current_index': 0, 'graphs': [base_graph.elements]}, descriptions[0], "", True, True, [], [], [], dash.no_update, True
+            else:
+                return [], {'current_index': 0, 'graphs': []}, "", "", True, True, [], [], [], dash.no_update, True
+
+        elif button_id == 'remove-all-productions-button':
+            return [], {'current_index': 0, 'graphs': []}, "", "", True, True, [], [], [], dash.no_update, True
+
+        elif button_id == 'import-productions-button':
+            if contents is None:
+                return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, True
+
+            content_type, content_string = contents.split(',')
+            decoded = base64.b64decode(content_string)
+            content = decoded.decode('utf-8')
+
+            parser = ProductionParser()
+            L, K, R = parser.parse_productions(content)
+            l_graph = Graph()
+            k_graph = Graph()
+            r_graph = Graph()
+            l_graph.from_nodes_edges(L.nodes, L.edges)
+            k_graph.from_nodes_edges(K.nodes, K.edges)
+            r_graph.from_nodes_edges(R.nodes, R.edges)
+            deterministic_layout(l_graph.graph)
+            deterministic_layout(k_graph.graph)
+            deterministic_layout(r_graph.graph)
+            
+            l_highlighted = highlit_left_elements_which_does_not_exist_in_right(l_graph.elements, k_graph.elements, 'to-remove')
+            r_highlighted = highlit_left_elements_which_does_not_exist_in_right(r_graph.elements, k_graph.elements, 'added')
+
+            return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, l_highlighted, k_graph.elements, r_highlighted, "", False
+
+        return dash.no_update, dash.no_update, "", "", True, True, l_elements, k_elements, r_elements, dash.no_update, True
 
     @app.callback(
         Output('main-graph', 'layout'),
@@ -155,83 +218,6 @@ def register_callbacks(app, base_graph):
         if ctx.triggered:
             return get_default_graph_layout()
         return dash.no_update
-    
-    @app.callback(
-        [Output('main-graph', 'elements', allow_duplicate=True),
-         Output('main-graph-data', 'data', allow_duplicate=True),
-         Output('graph-description', 'children', allow_duplicate=True),
-         Output('feedback', 'children', allow_duplicate=True),
-         Output('next-step-button', 'disabled'),
-         Output('previous-step-button', 'disabled')],
-        [Input('apply-production-button', 'n_clicks'),
-         Input('load-graph-button', 'n_clicks'),
-         Input('clear-graph-button', 'n_clicks'),
-         Input('next-step-button', 'n_clicks'),
-         Input('previous-step-button', 'n_clicks')],
-        [State('main-graph-data', 'data')],
-        prevent_initial_call=True
-    )
-    def next_graph_step(load_graph, clear_graph, apply_production, next_step, previous_step, graph_data):
-        print('update requested')
-        
-        def can_go_backward(graph_data):
-            return graph_data['current_index'] > 0
-        def can_go_forward(graph_data):
-            return graph_data['current_index'] < len(graph_data['graphs']) - 1
-        
-        ctx = dash.callback_context
-        if not ctx.triggered:
-            return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
-        print('no ctx')
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-        
-        if graph_data is None or 'graphs' not in graph_data:
-            return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
-
-        current_index = graph_data['current_index']
-        graphs = graph_data['graphs']
-
-        if button_id == 'next-step-button' and current_index + 1 < len(graphs) :
-            current_index = (current_index + 1)
-        elif button_id == 'previous-step-button' and current_index - 1 >= 0:
-            current_index = (current_index - 1)
-
-        graph_data['current_index'] = current_index
-        print('should update')
-        return graphs[current_index], graph_data, descriptions[current_index], "", can_go_forward(graph_data), can_go_backward(graph_data)
-
-    @app.callback(
-        [Output('graph-l', 'elements'),
-         Output('graph-k', 'elements'),
-         Output('graph-r', 'elements')],
-        [Input('import-productions-button', 'contents')],
-        prevent_initial_call=True
-    )
-    def import_productions(contents):
-        if contents is None:
-            return dash.no_update, dash.no_update, dash.no_update
-
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-        content = decoded.decode('utf-8')
-
-        parser = ProductionParser()
-        L, K, R = parser.parse_productions(content)
-        l_graph = Graph()
-        k_graph = Graph()
-        r_graph = Graph()
-        l_graph.from_nodes_edges(L.nodes, L.edges)
-        k_graph.from_nodes_edges(K.nodes, K.edges)
-        r_graph.from_nodes_edges(R.nodes, R.edges)
-        deterministic_layout(l_graph.graph)
-        deterministic_layout(k_graph.graph)
-        deterministic_layout(r_graph.graph)
-        
-        l_highlighted = highlit_left_elements_which_does_not_exist_in_right(l_graph.elements, k_graph.elements, 'to-remove')
-        r_highlighted = highlit_left_elements_which_does_not_exist_in_right(r_graph.elements, k_graph.elements, 'added')
-
-        return l_highlighted, k_graph.elements, r_highlighted
-    
 
 def highlit_subgraf_in_graph(base_graph_elements, subgraph_elements, tag):
     output_elements = []
