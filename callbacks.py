20a50f207c09c8327b9d5e5ef6fd2dc63165bb87
WojciechNeuman <wojciech.neuman@gmail.com>
2024-11-14 19:35:38 +0100

    refactor: conflicts resolved

--- callbacks.py
+++ callbacks.py
@@ -13,11 +13,20 @@ from networkx.algorithms.isomorphism import DiGraphMatcher
 import networkx as nx
 
 def register_callbacks(app, base_graph):
+    descriptions = [
+        "Base Graph (G)",
+        "Base Graph (G) with highlighted  m(L)",
+        "Base Graph (G) with highlighted m(L) - m(K)",
+        "Graph Z after removing m(L) - m(K)",
+        "Final Graph G with highlighted  m(R) - m(K)'",
+        "Final Graph G'",
+    ]
+    
     @app.callback(
-        [Output('main-graph', 'elements'),
-         Output('main-graph-data', 'data'),
-         Output('graph-description', 'children'),
-         Output('feedback', 'children')],
+        [Output('main-graph', 'elements', allow_duplicate=True),
+         Output('main-graph-data', 'data', allow_duplicate=True),
+         Output('graph-description', 'children', allow_duplicate=True),
+         Output('feedback', 'children', allow_duplicate=True)],
         [Input('add-node-button', 'n_clicks'),
          Input('add-edge-button', 'n_clicks'),
          Input('remove-selected-button', 'n_clicks'),
@@ -32,7 +41,8 @@ def register_callbacks(app, base_graph):
          State('graph-l', 'elements'),
          State('graph-k', 'elements'),
          State('graph-r', 'elements'),
-         State('main-graph-data', 'data')]
+         State('main-graph-data', 'data')],
+        prevent_initial_call=True
     )
     def update_graph(n_clicks_node, n_clicks_edge, n_clicks_remove, n_clicks_load, n_clicks_clear, n_clicks_apply, n_clicks_next, n_clicks_prev, elements, selected_nodes, selected_edges, l_elements, k_elements, r_elements, graph_data):
         ctx = dash.callback_context
@@ -40,15 +50,6 @@ def register_callbacks(app, base_graph):
             return elements, graph_data, "", ""
 
         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-        descriptions = [
-            "Base Graph (G)",
-            "Base Graph (G) with highlighted  m(L)",
-            "Base Graph (G) with highlighted m(L) - m(K)",
-            "Graph Z after removing m(L) - m(K)",
-            "Final Graph G with highlighted  m(R) - m(K)'",
-            "Final Graph G'",
-        ]
 
         if button_id == 'add-node-button':
             base_graph.copy_from(elements)
@@ -73,6 +74,7 @@ def register_callbacks(app, base_graph):
         elif button_id == 'load-graph-button':
             from app import GRAPH_FILE_PATH
             base_graph.from_csv(GRAPH_FILE_PATH)
+            base_graph.elements = add_lock_to_all_graph_elements(base_graph.elements)
             return base_graph.elements, graph_data, descriptions[0], ""
 
         elif button_id == 'clear-graph-button':
@@ -106,61 +108,40 @@ def register_callbacks(app, base_graph):
             mL_minus_mK = dpo.calculate_mL_minus_mK()
             graph_mL_minus_mK = Graph()
             graph_mL_minus_mK.multi_digraph_from_nodes_edges(mL_minus_mK.nodes, mL_minus_mK.edges)
+            graph_mL_minus_mK.elements = add_lock_to_all_graph_elements(graph_mL_minus_mK.elements)
 
             Z = dpo.calculate_Z(mL_minus_mK)
             graph_Z = Graph()
             graph_Z.multi_digraph_from_nodes_edges(Z.nodes, Z.edges)
+            graph_Z.elements = add_lock_to_all_graph_elements(graph_Z.elements)
 
             mR_minus_mK = dpo.calculate_mR_minus_mK()
             graph_mR_minus_mK = Graph()
             graph_mR_minus_mK.multi_digraph_from_nodes_edges(mR_minus_mK.nodes, mR_minus_mK.edges)
+            graph_mR_minus_mK.elements = add_lock_to_all_graph_elements(graph_mR_minus_mK.elements)
 
             G_prime = dpo.create_G_prime(Z, mR_minus_mK)
             graph_G_prime = Graph()
             graph_G_prime.multi_digraph_from_nodes_edges(G_prime.nodes, G_prime.edges)
+            graph_G_prime.elements = add_lock_to_all_graph_elements(graph_G_prime.elements)
 
 
             G_highlight_L = highlit_subgraf_in_graph(base_graph.elements, L.elements, 'added')
             G_highlight_L_minus_K = highlit_left_elements_which_does_not_exist_in_right(base_graph.elements, graph_Z.elements, 'to-remove')
             G_prime_highlited_mR_minus_mK = highlit_left_elements_which_does_not_exist_in_right(graph_G_prime.elements, graph_Z.elements, 'added')
 
+            G_highlight_L = add_lock_to_all_graph_elements(G_highlight_L)
+            G_highlight_L_minus_K = add_lock_to_all_graph_elements(G_highlight_L_minus_K)
+            G_prime_highlited_mR_minus_mK = add_lock_to_all_graph_elements(G_prime_highlited_mR_minus_mK)
 
             if not nx.is_weakly_connected(Z) or not nx.is_weakly_connected(G_prime):
                 return dash.no_update, dash.no_update, "Please check the input graphs. Cannot apply the DPO production.", "Please check the input graphs. Cannot apply the DPO production."            
 
-            graphs = [base_graph.graph, Z, G_prime]
-            graph_elements = []
-            for i, g in enumerate(graphs):
-                tmp_graph = Graph()
-                if i == 0:
-                    tmp_graph.from_nodes_edges(g.nodes, g.edges)
-                else:
-                    tmp_graph.multi_digraph_from_nodes_edges(g.nodes, g.edges)
-                graph_elements.append(tmp_graph.elements)
-
-            graph_elements.insert(1, G_highlight_L)
-            graph_elements.insert(2, G_highlight_L_minus_K)
-            graph_elements.insert(4, G_prime_highlited_mR_minus_mK)
+            graph_elements = [base_graph.elements, G_highlight_L, G_highlight_L_minus_K, graph_Z.elements, G_prime_highlited_mR_minus_mK, graph_G_prime.elements]
 
             graph_elements = [fill_classes_as_empty_if_does_not_exist(i) for i in graph_elements]                
             graph_data = {'current_index': 0, 'graphs': graph_elements}
-            print(graph_elements)
             return graph_elements[0], graph_data, descriptions[0], ""
-
-        elif button_id in ['next-step-button', 'previous-step-button']:
-            if graph_data is None or 'graphs' not in graph_data:
-                return dash.no_update, dash.no_update, dash.no_update, ""
-
-            current_index = graph_data['current_index']
-            graphs = graph_data['graphs']
-
-            if button_id == 'next-step-button':
-                current_index = (current_index + 1) % len(graphs)
-            elif button_id == 'previous-step-button':
-                current_index = (current_index - 1) % len(graphs)
-
-            graph_data['current_index'] = current_index
-            return graphs[current_index], graph_data, descriptions[current_index], ""
         return dash.no_update, dash.no_update, "", ""
 
     @app.callback(
@@ -173,6 +154,50 @@ def register_callbacks(app, base_graph):
         if ctx.triggered:
             return get_default_graph_layout()
         return dash.no_update
+    
+    @app.callback(
+        [Output('main-graph', 'elements', allow_duplicate=True),
+         Output('main-graph-data', 'data', allow_duplicate=True),
+         Output('graph-description', 'children', allow_duplicate=True),
+         Output('feedback', 'children', allow_duplicate=True),
+         Output('next-step-button', 'disabled'),
+         Output('previous-step-button', 'disabled')],
+        [Input('apply-production-button', 'n_clicks'),
+         Input('load-graph-button', 'n_clicks'),
+         Input('clear-graph-button', 'n_clicks'),
+         Input('next-step-button', 'n_clicks'),
+         Input('previous-step-button', 'n_clicks')],
+        [State('main-graph-data', 'data')],
+        prevent_initial_call=True
+    )
+    def next_graph_step(load_graph, clear_graph, apply_production, next_step, previous_step, graph_data):
+        print('update requested')
+        
+        def can_go_backward(graph_data):
+            return graph_data['current_index'] > 0
+        def can_go_forward(graph_data):
+            return graph_data['current_index'] < len(graph_data['graphs']) - 1
+        
+        ctx = dash.callback_context
+        if not ctx.triggered:
+            return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
+        print('no ctx')
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+        
+        if graph_data is None or 'graphs' not in graph_data:
+            return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
+
+        current_index = graph_data['current_index']
+        graphs = graph_data['graphs']
+
+        if button_id == 'next-step-button' and current_index + 1 < len(graphs) :
+            current_index = (current_index + 1)
+        elif button_id == 'previous-step-button' and current_index - 1 >= 0:
+            current_index = (current_index - 1)
+
+        graph_data['current_index'] = current_index
+        print('should update')
+        return graphs[current_index], graph_data, descriptions[current_index], "", can_go_forward(graph_data), can_go_backward(graph_data)
 
     @app.callback(
         [Output('graph-l', 'elements'),
@@ -200,8 +225,11 @@ def register_callbacks(app, base_graph):
         deterministic_layout(l_graph.graph)
         deterministic_layout(k_graph.graph)
         deterministic_layout(r_graph.graph)
+        
+        l_highlighted = highlit_left_elements_which_does_not_exist_in_right(l_graph.elements, k_graph.elements, 'to-remove')
+        r_highlighted = highlit_left_elements_which_does_not_exist_in_right(r_graph.elements, k_graph.elements, 'added')
 
-        return l_graph.elements, k_graph.elements, r_graph.elements
+        return l_highlighted, k_graph.elements, r_highlighted
     
 
 def highlit_subgraf_in_graph(base_graph_elements, subgraph_elements, tag):
@@ -264,3 +292,11 @@ def fill_classes_as_empty_if_does_not_exist(elements):
         if 'classes' not in x.keys():
              elements[i]['classes'] = ''
     return elements
+
+def add_lock_to_all_graph_elements(elements):
+    new_elements = []
+    for i in elements:
+        z = i.copy()
+        z['locked'] = True
+        new_elements.append(z)
+    return new_elements
